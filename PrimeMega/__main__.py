e06cc87662aa1a9d49f21efccb2cba273c0b9883
kapii04 <119713755+kapii04@users.noreply.github.com>
2022-12-27 12:23:44 +0700

    Update __main__.py

--- PrimeMega/__main__.py
+++ PrimeMega/__main__.py
@@ -86,7 +86,9 @@ def get_readable_time(seconds: int) -> str:
 PM_START_TEXT = """
 *Hello {} !*
 ────────────────────────
-I'm a powerful group management bot built to help you manage your group!
+sᴀʏᴀ ᴀᴅᴀʟᴀʜ ʙᴏᴛ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ x ᴍᴜsɪᴄ 
+ʏᴀɴɢ ʙɪsᴀ ᴋᴀᴍᴜ ᴘᴀᴋᴇ ᴅɪ ɢʀᴜᴘ ᴋᴀᴍᴜ
+ᴏᴡɴᴇʀ @mocpi
 ────────────────────
 Hit the /help or tap on button to se available command on Me.
 """
@@ -114,10 +116,10 @@ buttons = [
 HELP_STRINGS = """
 Click on the button bellow to get description about specifics command."""
 
-PRIME_IMG = "https://telegra.ph/file/b9fa4e1360d0a872a3cd5.jpg"
+PRIME_IMG = "https://telegra.ph/file/b1b23511a14f29ba327da.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
- You can support the project by contacting @Bukan_guudlooking \
+ You can support the project by contacting @mocpi \
  Supporting isnt always financial! \
  Those who cannot provide monetary support are welcome to help us develop the bot at ."""
 
